1ab8f3022a601e4567283024a9c55c6f6c5cb64a
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-11 20:00:49 -0800

    forgot to add pages/

---
+++ pages/project.js
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+import ProjectPageLeft from "../src/app/components/ProjectPageLeft.jsx";
+
+export default function Project() {
+  return (
+    <>
+      <div id="project-page-container" className="f">
+        <ProjectPageLeft />
+      </div>
+    </>
+  );
+}
