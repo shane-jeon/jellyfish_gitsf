df8a09e21dcd576e1ef3fe9d4602d775fbc9b8b7
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-11 20:02:35 -0800

    Resolving merge conflict by copy and pasting ActiveProject code from main to 'shane-branch'

--- src/app/components/ActiveProject.jsx
+++ src/app/components/ActiveProject.jsx
@@ -1,12 +1,26 @@
-import React from "react";
-import Image from "next/image"
-import younggirl from "../../../public/assets/younggirl.png"
+export default function ActiveProject({ progressPercent }) {
+  return (
+    <div className="w-6/8 relative mx-6 mt-2 rounded-xl">
+      <Image src={activeProjectGirl} />
 
-// props for number of girls entering whatever grade
+      <div className="absolute inset-0 rounded-xl bg-gradient-to-r from-[#25348F] via-[#25348F]  to-transparent opacity-60"></div>
 
-export default function ActiveProject() {
-  return(
-    <>
-    </>
-  )
+      <div className="absolute top-0 w-full">
+        <div className="mx-3 my-2 flex flex-row items-center">
+          <div className="h-6 w-6 rounded-full bg-[#D9D9D9]"></div>
+          <p className="mx-3 text-[#FFFFFF]">
+            Together with <span>72</span> donors
+          </p>
+        </div>
+        <h3 className="m-3 text-xl font-semibold text-[#FFFFFF]">
+          Launching <br />
+          Disabled Kids <br />
+          Program
+        </h3>
+        <span className="mx-3 flex justify-end text-3xl text-xl font-semibold text-[#FFFFFF]">
+          {progressPercent}
+        </span>
+      </div>
+    </div>
+  );
 }
