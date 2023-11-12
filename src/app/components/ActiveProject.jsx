1a76b17cba777c0b385797e25a29fb097e7e76f1
hicass <cassidyjwalters@gmail.com>
2023-11-11 19:39:27 -0800

    Add content to ActiveProject components, and style it.

--- src/app/components/ActiveProject.jsx
+++ src/app/components/ActiveProject.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Image from "next/image"
-import younggirl from "../../../public/assets/younggirl.png"
+import React from 'react';
+import Image from 'next/image';
+import activeProjectGirl from '../../../public/assets/active-project-girl.png';
 
 // props for number of girls entering whatever grade
 
@@ -9,12 +9,12 @@ export default function ActiveProject() {
     <>
     <div className="relative">
       <Image src={younggirl} />
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-b from-transparent to-white"></div>
-    </div>
+
       <div className="absolute bottom-5 w-52">
         {/* replace with prop for future implementation */}
         <h2 className="text-[#FFFFFF] text-xl">9 girls entering 11th grade</h2>
       </div>
+    </div>
     </>
   )
 }
