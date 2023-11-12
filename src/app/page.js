3a67392a2244f634022e482cecab2f71f0797d3d
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-11 18:54:28 -0800

    Fixed ActiveProject back

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import DonorProfile from 'src/app/components/DonorProfile.jsx'
 import NavBar from 'src/app/components/NavBar.jsx'
-import UpcomingProject from 'src/app/components/UpcomingProject.jsx'
+import ActiveProject from 'src/app/components/ActiveProject.jsx'
 
 export default function Home() {
   return (
     <>
       {/* <Hero /> */}
       <DonorProfile />
+      <ActiveProject />
       {/* <Footer /> */}
     </>
   )
