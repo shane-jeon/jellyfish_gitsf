d71b8a05990eb6fa54a86a963d20262b3097ca38
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-11 19:37:57 -0800

    first commit for new shane-branch

---
+++ src/app/pages/project.js
@@ -0,0 +1,11 @@
+import ProjectPageLeft from "../src/app/components/ProjectPageLeft.jsx";
+
+export default function Project() {
+  return (
+    <>
+      <div id="project-page-container" className="f">
+        <ProjectPageLeft />
+      </div>
+    </>
+  );
+}
